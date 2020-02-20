619a70a184a50617462f1416e521d6d410673f6a
lucas <lucas.camacho@soulasalle.com.br>
2020-02-20 15:32:35 -0300

    teste

---
+++ raio.py
@@ -0,0 +1,27 @@
+import math
+
+def area_do_circulo(raio):
+    
+    area = (math.pi) * pow(raio,2)
+    return area
+
+def comprimento_do_circulo(raio):
+    comprimento = 2 *  (math.pi * raio)
+    return comprimento
+
+
+i = 0
+while i == 0:
+    try:
+        raio = float(input('Digite o raio do circulo: '))
+    except ValueError:
+        print('Digite um FLOAT ANIMAL!!')
+    else:
+        i = 1
+
+result_area = area_do_circulo(raio)
+result_comprimento = comprimento_do_circulo(raio)
+
+print('A área do circulo é: {}'.format(result_area))
+print('O comprimento do circulo é: {}'.format(result_comprimento))
+
